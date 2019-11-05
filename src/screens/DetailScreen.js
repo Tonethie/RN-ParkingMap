8a71b43569b0a249bc10dce0a5ae930e982b99d0
Tonethie <luiz_fcarvalho@hotmail.com>
2019-11-05 15:22:04 -0300

    Update DetailScreen.js

--- src/screens/DetailScreen.js
+++ src/screens/DetailScreen.js
@@ -1,16 +1,19 @@
 import React, { Component } from "react";
 import { 
+    Alert,
     BackHandler,
     View,
     Text,
     ScrollView,
     ImageBackground,
     StyleSheet,
-    StatusBar
+    StatusBar,
+    TouchableOpacity
 } from "react-native";
 import { createBottomTabNavigator } from 'react-navigation'
 import Icon from 'react-native-vector-icons/Ionicons'
 import { Container, Header,Left, Body, Right, Title, Button, Subtitle, Content, Card, CardItem } from 'native-base'
+import { CreditCardInput, LiteCreditCardInput } from "react-native-credit-card-input";
 
 export class DetailScreen extends Component {
     render() {
@@ -195,11 +198,75 @@ export class Estacionamento3 extends Component {
         );
     }
 }
+export class CartaoCredito extends Component {
+    state = { useLiteCreditCardInput: false,
+                AddedCard:false };
+    _onChange = (formData) => console.log(JSON.stringify(formData, null, " "));
+    _onFocus = (field) => console.log("focusing", field);
+    _setUseLiteCreditCardInput = (useLiteCreditCardInput) => this.setState({ useLiteCreditCardInput });
+    
+    constructor(props){
+        super(props);
+        this.handleAddClick = this.handleAddClick.bind(this);
+        this.state = {AddedCard: false};
+    }
+    handleAddClick(){
+        this.setState({AddedCard: true});
+        alert('Adicionado');
+    }
 
+    componentWillMount()
+    {
+        BackHandler.addEventListener('hardwareBackPress', ()=>this.props.navigation.navigate('Pay'));
+    }
+    render() {
+        return (
+            <Container>
+            <Header style={{backgroundColor:'#980e0e', borderColor:'black'} }>
+            <StatusBar backgroundColor='#841515' barStyle='light-content' />
+                <Left>
+                <Button transparent onPress={()=>this.props.navigation.navigate('Pay')}>
+                    <Icon name='md-arrow-back' size={24} color='#fff' />
+                </Button>
+                </Left>
+                <Body>
+                <Title style={{justifyContent:'center'}}>Add Cartão de Crédito</Title>
+                </Body>
+            </Header>
+            <View style={styles.container}>
+                <CreditCardInput
+                        autoFocus
+                        requiresName
+                        requiresCVC
+                        requiresPostalCode
+
+                        labelStyle={styles.label}
+                        inputStyle={styles.input}
+                        validColor={"black"}
+                        invalidColor={"red"}
+                        placeholderColor={"darkgray"}
+
+                        onFocus={this._onFocus}
+                        onChange={this._onChange} />
+            </View>
+            <View style={{paddingVertical:20,
+                        alignItems:'center',
+                        justifyContent: 'flex-end'}}>
+                <TouchableOpacity style={styles.button}
+                        onPress={this.handleAddClick}>
+                        <Text style={styles.buttonText}>Adicionar</Text>
+                </TouchableOpacity>
+            </View>
+        
+            </Container>
+        );
+    }
+}
 export default createBottomTabNavigator({
     Park1:Estacionamento1,
     Park2:Estacionamento2,
     Park3:Estacionamento3,
+    AddCard:CartaoCredito,
 },{
     navigationOptions:{
         tabBarVisible: false,
@@ -209,6 +276,7 @@ export default createBottomTabNavigator({
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        paddingVertical:20,
         alignItems: 'center',
         justifyContent: 'center'
     },
@@ -220,5 +288,20 @@ const styles = StyleSheet.create({
         width:160,
         height: 160,
         backgroundColor: 'rgba(255,200,255, .8)'
+    },
+    button: {
+        width: 200,
+        backgroundColor: '#c21e1e',
+        borderRadius: 25,
+        marginVertical:10,
+        paddingHorizontal: 16,
+    },
+
+    buttonText: {
+        fontSize: 16,
+        fontWeight:'500',
+        color: '#fff',
+        marginVertical:5,
+        textAlign: 'center'
     }
 });
